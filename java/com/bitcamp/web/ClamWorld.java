9d3b0d017e08317bf788310386266ef3442adce1
JangWonJong <jwj9922@naver.com>
2022-01-19 11:43:39 +0900

    2022-01-19 11:38 id pw name 앱

---
+++ java/com/bitcamp/web/ClamWorld.java
@@ -0,0 +1,22 @@
+package com.bitcamp.web;
+
+import java.util.Scanner;
+
+public class ClamWorld {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("조개");
+        String 조개 = scanner.next();
+        System.out.println("가리비");
+        String 가리비 = scanner.next();
+        System.out.println("홍합");
+        String 홍합 = scanner.next();
+        System.out.println("크라켄");
+        String 크라켄 = scanner.next();
+        System.out.println("환영합니다 " + 크라켄 + "님 ");
+
+
+
+    }
+}
---
+++ java/com/bitcamp/web/HelloWorldApp.java
@@ -0,0 +1,29 @@
+package com.bitcamp.web;
+
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+public class HelloWorldApp {
+
+    // Real-world objects share two characteristics: They all have state and behavior.
+
+    /* state -> variable */
+
+    // 변수명은 name 이고 값은 철수인 변수를 선언하라
+    // 메소드명은 main 이고 누구나 볼 수 있도록 선언하라
+    // 철수나이는 20살이다
+    String name = "원종";
+    int age = 20;
+    public static void main(String[] args){
+
+    }
+
+
+
+
+    /* behavior -> function || method*/
+
+
+
+}
